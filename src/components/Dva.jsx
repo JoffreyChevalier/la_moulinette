6849698644aaafbf9efbc7b99d7cad7a747b412e
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-08-02 12:41:55 +0200

    fixed wrong calculation

--- src/components/Dva.jsx
+++ src/components/Dva.jsx
@@ -17,7 +17,7 @@ function Dva() {
   };
 
   const grossMarginCalcul = (costPrice, sellingPrice) => {
-    const number = (costPrice / sellingPrice - 1) * 100;
+    const number = (1 - costPrice / sellingPrice) * 100;
     return number.toFixed(2);
   };
 
@@ -28,27 +28,43 @@ function Dva() {
 
   return (
     <>
-      <h1 className="text-4xl text-center font-bold mb-10">Marge Brut</h1>
+      <h1 className="text-4xl text-center font-bold mb-10 underline text-primary">
+        Marge Brut
+      </h1>
       <div className="flex flex-col items-center">
         <form
           className="flex flex-col items-center space-y-6"
           onSubmit={handleSubmit(onSubmit)}
         >
-          <Input label="Prix de revient" {...register("costPrice")} />
-          <Input label="Prix de vente" {...register("sellingPrice")} />
-          <Button size="sm" type="submit">
+          <Input
+            label="Prix de revient"
+            pattern="[0-9]+"
+            title="Uniquement des chiffres"
+            required
+            {...register("costPrice")}
+          />
+          <Input
+            label="Prix de vente"
+            pattern="[0-9]+"
+            title="Uniquement des chiffres"
+            required
+            {...register("sellingPrice")}
+          />
+          <Button size="sm" type="submit" className="bg-primary">
             Calcule
           </Button>
         </form>
         <div className="flex items-center justify-center mt-6">
-          <div className="flex items-center justify-center w-40 h-10 border border-blue-600 rounded-l-lg">
-            <p className="text-center">
-              {grossMarginResult === NaN ? "---" : grossMarginResult}
-            </p>
+          <div className="flex items-center justify-center w-40 h-10 border border-primary rounded-l-lg">
+            {isNaN(grossMarginResult) ? (
+              <p className="text-center">---</p>
+            ) : (
+              <p className="text-center">{grossMarginResult}</p>
+            )}
           </div>
           <div>
             <IconButton
-              className="rounded-none rounded-r-lg"
+              className="rounded-none rounded-r-lg bg-primary"
               onClick={() => (
                 setCopied,
                 alert(`${grossMarginResult} copié dans le presse-papier`)
